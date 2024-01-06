8c4fcd99a0e7cb6452d87c37bae258e735dd0d0c
EP\adza <adnan.zameer@episerver.com>
2024-01-06 17:39:31 +0000

    mirror logic

--- src/SeoBoost/Business/Url/UrlBuilder.cs
+++ src/SeoBoost/Business/Url/UrlBuilder.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using EPiServer;
 using EPiServer.Core;
-using EPiServer.Core.Internal;
 using EPiServer.Web;
 using EPiServer.Web.Routing;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 using SeoBoost.Models;
 
@@ -29,64 +26,83 @@ namespace SeoBoost.Business.Url
 
         public string ContentExternalUrl(ContentReference contentLink, CultureInfo contentLanguage)
         {
-            string result = string.Empty;
+            var loadingOptions = new LoaderOptions { LanguageLoaderOption.FallbackWithMaster(contentLanguage) };
+            var pageData = _contentRepository.Get<PageData>(contentLink, loadingOptions);
 
+            var actualPage = IsMirrored(pageData) ?
+            GetMirroredTarget(pageData, contentLanguage) ?? pageData :
+            pageData;
+
+
+            var result = string.Empty;
             if (_configuration.UseSimpleAddress)
             {
-                var loadingOptions = new LoaderOptions { LanguageLoaderOption.FallbackWithMaster(contentLanguage) };
-
-                var pageData = _contentRepository.Get<PageData>(contentLink, loadingOptions);
-
-                if (pageData != null && !string.IsNullOrEmpty(pageData.ExternalURL))
+                if (actualPage != null && !string.IsNullOrEmpty(actualPage.ExternalURL))
                 {
                     result = pageData.ExternalURL;
                 }
             }
-
-            if (string.IsNullOrEmpty(result))
+            if (actualPage != null)
             {
-                result = _urlResolver.GetUrl(
-                    contentLink,
-                    contentLanguage.Name,
-                    new VirtualPathArguments
-                    {
-                        ContextMode = ContextMode.Default,
-                        ForceCanonical = true
-                    });
-            }
-
-            if (!Uri.TryCreate(result, UriKind.RelativeOrAbsolute, out var relativeUri)) return result;
-
-            if (relativeUri.IsAbsoluteUri) return result;
-
-            Uri baseUri = null;
-            if (!_configuration.UseSiteUrlAsDefault)
-            {
-                var siteDefinition = _siteDefinitionResolver.GetByContent(contentLink, true, true);
-                var hosts = siteDefinition.GetHosts(contentLanguage, true).ToList();
-
-                var host = hosts.FirstOrDefault(h => h.Type == HostDefinitionType.Primary && h.Language.Equals(contentLanguage));
-
-                baseUri = siteDefinition.SiteUrl;
-
-                if (host != null && host.Name.Equals("*") == false)
+                if (string.IsNullOrEmpty(result))
                 {
-                    Uri.TryCreate(siteDefinition.SiteUrl.Scheme + "://" + host.Name, UriKind.Absolute, out baseUri);
+                    result = _urlResolver.GetUrl(
+                        actualPage.ContentLink,
+                        contentLanguage.Name,
+                        new VirtualPathArguments
+                        {
+                            ContextMode = ContextMode.Default,
+                            ForceCanonical = true
+                        });
                 }
-            }
-            if (baseUri == null)
-            {
-                Uri.TryCreate(SiteDefinition.Current.SiteUrl.AbsoluteUri, UriKind.Absolute, out baseUri);
-            }
 
-            if (baseUri != null)
-            {
-                var absoluteUri = new Uri(baseUri, relativeUri);
+                if (!Uri.TryCreate(result, UriKind.RelativeOrAbsolute, out var relativeUri)) return result;
 
-                return absoluteUri.AbsoluteUri;
+                if (relativeUri.IsAbsoluteUri) return result;
+
+                Uri baseUri = null;
+                if (!_configuration.UseSiteUrlAsDefault)
+                {
+                    var siteDefinition = _siteDefinitionResolver.GetByContent(actualPage.ContentLink, true, true);
+                    var hosts = siteDefinition.GetHosts(contentLanguage, true).ToList();
+
+                    var host = hosts.FirstOrDefault(h => h.Type == HostDefinitionType.Primary && h.Language.Equals(contentLanguage));
+
+                    baseUri = siteDefinition.SiteUrl;
+
+                    if (host != null && host.Name.Equals("*") == false)
+                    {
+                        Uri.TryCreate(siteDefinition.SiteUrl.Scheme + "://" + host.Name, UriKind.Absolute, out baseUri);
+                    }
+                }
+                if (baseUri == null)
+                {
+                    Uri.TryCreate(SiteDefinition.Current.SiteUrl.AbsoluteUri, UriKind.Absolute, out baseUri);
+                }
+
+                if (baseUri != null)
+                {
+                    var absoluteUri = new Uri(baseUri, relativeUri);
+
+                    return absoluteUri.AbsoluteUri;
+                }
             }
 
             return string.Empty;
+        }
+
+        private PageData GetMirroredTarget(PageData currentPage, CultureInfo contentLanguage)
+        {
+            var target = (PropertyPageReference)currentPage.Property["PageShortcutLink"];
+            var loadingOptions = new LoaderOptions { LanguageLoaderOption.FallbackWithMaster(contentLanguage) };
+            var targetPage = _contentRepository.Get<PageData>(target.PageLink, loadingOptions);
+
+            return targetPage ?? currentPage;
+        }
+
+        private static bool IsMirrored(PageData page)
+        {
+            return page.LinkType == PageShortcutType.FetchData || page.LinkType == PageShortcutType.Shortcut;
         }
     }
 }
